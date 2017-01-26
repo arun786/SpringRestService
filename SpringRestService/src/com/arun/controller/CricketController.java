ab828ac6e4de79612dc20f2aed963d10ac4dbcd4
arun789 <adwiti1975@gmail.com>
2017-01-25 23:15:31 -0700

    Spring Rest service

--- SpringRestService/src/com/arun/controller/CricketController.java
+++ SpringRestService/src/com/arun/controller/CricketController.java
@@ -1,24 +1,34 @@
 package com.arun.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.arun.bean.Country;
+
+@RestController
 @RequestMapping("/cricket")
 public class CricketController {
 
-	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
-	public String getCricketer(@PathVariable String name, ModelMap model) {
-		model.addAttribute("cricket", name);
-		return "list";
+	/* url : localhost:8080/SpringRestService/cricket/ */
+
+	@RequestMapping(value = "/", method = RequestMethod.GET, headers = "Accept=application/json")
+	public List<Country> getDefaultCricketer() {
+
+		Country c1 = new Country("1", "India");
+		Country c2 = new Country("2", "Australia");
+
+		List<Country> lstC1 = new ArrayList<>();
+		lstC1.add(c1);
+		lstC1.add(c2);
+		return lstC1;
 	}
 
-	@RequestMapping(value="/", method = RequestMethod.GET)
-	public String getDefaultCricketer(ModelMap map) {
-		map.addAttribute("cricket", "Sachin");
-		return "list";
-	}
+	/*
+	 * o/p of the above method will be
+	 * [{"id":"1","name":"India"},{"id":"2","name":"Australia"}]
+	 */
 }
