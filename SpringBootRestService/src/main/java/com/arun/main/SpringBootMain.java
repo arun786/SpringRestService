5d467bfd70c61c6ec93bd93d511bc5c939a081de
arun789 <adwiti1975@gmail.com>
2017-01-30 00:55:07 -0700

    to change jar file to war file

--- SpringBootRestService/src/main/java/com/arun/main/SpringBootMain.java
+++ SpringBootRestService/src/main/java/com/arun/main/SpringBootMain.java
@@ -2,13 +2,27 @@ package com.arun.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @EnableAutoConfiguration
 @Configuration
 @ComponentScan(basePackages = { "com.arun.controller", "com.arun.service", "com.arun.dao"})
-public class SpringBootMain {
+public class SpringBootMain extends SpringBootServletInitializer{
+	
+	/*
+	 * To create a war file 
+	 * step 1 Extends SpringBootServletInitializer 
+	 * step 2 override the configure method 
+	 * step 3 change the jar file to war file in pom
+	 */
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringBootMain.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMain.class, args);
